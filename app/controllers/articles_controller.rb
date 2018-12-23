33a41b5fa3f1c54626355038fbf0bee388bdb78b
Nicolas <ing.nionate@gmail.com>
2018-12-23 16:16:09 -0300

    Adds form validation and update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,46 @@
 class ArticlesController < ApplicationController
 
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    # why need to be a new instance?, how it get the validation errors?
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new' #this means that is called the view only?
     end
 
-    def show
-        @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+     redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
-    def new
-        
-    end
+  private
 
-    def create
-        @article = Article.new(article_params)
-
-        @article.save
-
-        redirect_to @article
-    end
-
-    private
-    def article_params
-        params.require(:article).permit(:title, :text)
-    end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
